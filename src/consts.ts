d934eade645bc61cc04aaf4e7ad3ecd5bb93e31c
Felix Roos <flix91@gmail.com>
2023-05-19 00:44:06 +0200

    delete most things, add favicon

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Loophole Letters';
+export const SITE_DESCRIPTION = 'Welcome to my blog! This is where I write about music and coding stuff that I find interesting.';
