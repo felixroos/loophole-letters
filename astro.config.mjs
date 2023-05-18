2e9be2327ca8f60bc3ca977b4103bdd03f301281
Felix Roos <flix91@gmail.com>
2023-05-19 01:25:18 +0200

    migrate layout + styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://loophole-letters.netlify.app',
-	integrations: [mdx(), sitemap()],
+  site: 'https://loophole-letters.netlify.app',
+  integrations: [mdx(), sitemap(), tailwind()]
 });
