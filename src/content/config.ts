f32316dc04c018ce95efbb20fd23c32cb3fc8abd
Felix Roos <flix91@gmail.com>
2023-05-23 23:06:16 +0200

    ignore draft posts

--- src/content/config.ts
+++ src/content/config.ts
@@ -16,6 +16,7 @@ const blog = defineCollection({
       .transform((str) => (str ? new Date(str) : undefined)),
     image: z.string().optional(),
     tags: z.array(z.string()).optional(),
+    draft: z.boolean().optional(),
   }),
 });
 
