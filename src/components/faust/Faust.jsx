aa1a1fa39ec40e0d44fe8b03027ff159b346fa42
Felix Roos <flix91@gmail.com>
2023-09-06 15:27:14 +0200

    load stuff only once

--- src/components/faust/Faust.jsx
+++ src/components/faust/Faust.jsx
@@ -2,49 +2,43 @@ import dspModuleUrl from "./reverb/dspModule.wasm?url";
 import dspMeta from "./reverb/dspMeta.json";
 import { FaustMonoDspGenerator } from "@grame/faustwasm";
 
-let clapBuffer;
-async function getClapBuffer(ac) {
-  const load = async () => {
-    // create buffer source from a test sample
-    const res = await fetch(
-      "https://raw.githubusercontent.com/tidalcycles/Dirt-Samples/master/cp/HANDCLP0.wav"
-    );
-    const buffer = await res.arrayBuffer();
-    const audioBuffer = await ac.decodeAudioData(buffer);
-    return audioBuffer;
-  };
-  if (!clapBuffer) {
-    clapBuffer = load();
-  }
-  return clapBuffer;
+async function loadClapBuffer(ac) {
+  const res = await fetch(
+    "https://raw.githubusercontent.com/tidalcycles/Dirt-Samples/master/cp/HANDCLP0.wav"
+  );
+  const buffer = await res.arrayBuffer();
+  const audioBuffer = await ac.decodeAudioData(buffer);
+  return audioBuffer;
 }
 
-let audioContext;
-const getAudioContext = () => audioContext || new AudioContext();
+const loadFaustReverb = async (ac) => {
+  const module = await WebAssembly.compileStreaming(fetch(dspModuleUrl));
+  const generator = new FaustMonoDspGenerator();
+  return generator.createNode(ac, "FaustMonoDSP", {
+    module,
+    json: JSON.stringify(dspMeta),
+  });
+};
+
+let audioContext, clapBuffer, reverb;
 
 async function dry() {
-  const ac = new AudioContext();
+  const ac = audioContext || new AudioContext();
+
   const source = ac.createBufferSource();
-  source.buffer = await getClapBuffer(ac);
+  source.buffer = clapBuffer || (await loadClapBuffer(ac));
+
   source.connect(ac.destination);
   source.start();
 }
 
 async function wet() {
-  const ac = getAudioContext();
-
-  // init faustNode
-  const module = await WebAssembly.compileStreaming(fetch(dspModuleUrl));
-  const generator = new FaustMonoDspGenerator();
-  const faustNode = await generator.createNode(ac, "FaustMonoDSP", {
-    module,
-    json: JSON.stringify(dspMeta),
-  });
+  const ac = audioContext || new AudioContext();
 
   const source = ac.createBufferSource();
-  source.buffer = await getClapBuffer(ac);
+  source.buffer = clapBuffer || (await loadClapBuffer(ac));
+  const faustNode = reverb ? reverb : await loadFaustReverb(ac);
 
-  // connect it together and play
   source.connect(faustNode);
   faustNode.connect(ac.destination);
   source.start();
