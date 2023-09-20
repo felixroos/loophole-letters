d49316dd4156d122b332d6232f6dfe1fa0074839
Felix Roos <flix91@gmail.com>
2023-09-20 19:22:05 +0200

    trying assemblyscript

---
+++ src/components/asc/AssemblyScript.jsx
@@ -0,0 +1,38 @@
+import asc from "assemblyscript/dist/assemblyscript";
+
+export function AssemblyScript() {
+  async function run() {
+    //const asc = await import("assemblyscript/dist/assemblyscript");
+    console.log("asc", asc);
+    const { error, stdout, stderr, stats } = await asc.main(
+      [
+        // Command line options
+        "myModule.ts",
+        "--outFile",
+        "myModule.wasm",
+        "--optimize",
+        "--sourceMap",
+        "--stats",
+      ],
+      {
+        // Additional API options
+        /* stdout?: ...,
+      stderr?: ...,
+      readFile?: ...,
+      writeFile?: ...,
+      listFiles?: ...,
+      reportDiagnostic?: ...,
+      transforms?: ... */
+      }
+    );
+    if (error) {
+      console.log("Compilation failed: " + error.message);
+      console.log(stderr.toString());
+    } else {
+      console.log(stdout.toString());
+    }
+  }
+  run();
+
+  return <p>hello</p>;
+}
